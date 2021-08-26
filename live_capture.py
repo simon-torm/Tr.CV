4d5c856b6864f74d964cac81826ee7bd580fd318
sevmac <sivam.evt@gmail.com>
2021-08-26 17:56:31 +0300

    Added haar cascade recognition

--- live_capture.py
+++ live_capture.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import pkg_resources
 
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
@@ -9,6 +10,10 @@ cap_width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
 cap_height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
 print(f"{cap_width} x {cap_height}")
 
+# path = pkg_resources.resource_filename('cv2', 'haarcascade_frontalface_default.xml')
+# cv.CascadeClassifier().load('data/haarcascade_frontalface_default.xml')
+# print(path)
+classifier = cv.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = cap.read()
@@ -16,10 +21,17 @@ while True:
         print("Some problems accessing the camera")
         break
 
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    faces = classifier.detectMultiScale(frame, 1.3, 8)
+    # print(r)
+
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 200, 0), 3)
     cv.imshow("camera", frame)
 
-    if cv.waitKey(1) == ord('q'):
+
+    if cv.waitKey(10) == ord('q'):
         break
 
 cap.release()
